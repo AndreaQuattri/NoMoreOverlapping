643c0c878fe17627ec007e3a1a4d5b0eb467dd07
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-04-22 23:24:27 +0200

    update visible: deleted final

--- src/urlPhp/GiveAll.java
+++ src/urlPhp/GiveAll.java
@@ -2,20 +2,21 @@ package urlPhp;
 
 public class GiveAll {
 	
-	public static final String giveAllAttività = "http://nomoreoverlapping.altervista.org/giveAll/giveAllAttivita.php";
-	public static final String giveAllAula = "http://nomoreoverlapping.altervista.org/giveAll/giveAllAula.php";
-	public static final String giveAllConvegno = "http://nomoreoverlapping.altervista.org/giveAll/giveAllConvegno.php";
-	public static final String giveAllCorsoDiStudi = "http://nomoreoverlapping.altervista.org/giveAll/giveAllCorsoDiStudi.php";
-	public static final String giveAllDisciplina = "http://nomoreoverlapping.altervista.org/giveAll/giveAllDisciplina.php";
-	public static final String giveAllDocente = "http://nomoreoverlapping.altervista.org/giveAll/giveAllDocente.php";
-	public static final String giveAllEsame = "http://nomoreoverlapping.altervista.org/giveAll/giveAllEsame.php";
-	public static final String giveAllFasciaOraria = "http://nomoreoverlapping.altervista.org/giveAll/giveAllFasciaOraria.php";
-	public static final String giveAllGita = "http://nomoreoverlapping.altervista.org/giveAll/giveAllGita.php";
-	public static final String giveAllInsegna = "http://nomoreoverlapping.altervista.org/giveAll/giveAllInsegna.php";
-	public static final String giveAllPianoDiStudio = "http://nomoreoverlapping.altervista.org/giveAll/giveAllPianoDiStudio.php";
-	public static final String giveAllPreferenzaAula = "http://nomoreoverlapping.altervista.org/giveAll/giveAllPreferenzaAula.php";
-	public static final String giveAllPreferenzaFasciaOraria = "http://nomoreoverlapping.altervista.org/giveAll/giveAllPreferenzaFasciaOraria.php";
-	public static final String giveAllStudente = "http://nomoreoverlapping.altervista.org/giveAll/giveAllStudente.php";
-	public static final String giveAllTirocinio = "http://nomoreoverlapping.altervista.org/giveAll/giveAllTirocinio.php";
-
+	public static String giveAllAttività = "http://nomoreoverlapping.altervista.org/giveAll/giveAllAttivita.php";
+	public static String giveAllAula = "http://nomoreoverlapping.altervista.org/giveAll/giveAllAula.php";
+	public static String giveAllConvegno = "http://nomoreoverlapping.altervista.org/giveAll/giveAllConvegno.php";
+	public static String giveAllCorsoDiStudi = "http://nomoreoverlapping.altervista.org/giveAll/giveAllCorsoDiStudi.php";
+	public static String giveAllDisciplina = "http://nomoreoverlapping.altervista.org/giveAll/giveAllDisciplina.php";
+	public static String giveAllDocente = "http://nomoreoverlapping.altervista.org/giveAll/giveAllDocente.php";
+	public static String giveAllEsame = "http://nomoreoverlapping.altervista.org/giveAll/giveAllEsame.php";
+	public static String giveAllFasciaOraria = "http://nomoreoverlapping.altervista.org/giveAll/giveAllFasciaOraria.php";
+	public static String giveAllGita = "http://nomoreoverlapping.altervista.org/giveAll/giveAllGita.php";
+	public static String giveAllInsegna = "http://nomoreoverlapping.altervista.org/giveAll/giveAllInsegna.php";
+	public static String giveAllPianoDiStudio = "http://nomoreoverlapping.altervista.org/giveAll/giveAllPianoDiStudio.php";
+	public static String giveAllPreferenzaAula = "http://nomoreoverlapping.altervista.org/giveAll/giveAllPreferenzaAula.php";
+	public static String giveAllPreferenzaFasciaOraria = "http://nomoreoverlapping.altervista.org/giveAll/giveAllPreferenzaFasciaOraria.php";
+	public static String giveAllStudente = "http://nomoreoverlapping.altervista.org/giveAll/giveAllStudente.php";
+	public static String giveAllTirocinio = "http://nomoreoverlapping.altervista.org/giveAll/giveAllTirocinio.php";
+	
+	
 }

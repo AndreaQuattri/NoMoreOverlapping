c10810a2d0970c383a01756a71141cd9924dd6f4
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-04-22 23:27:17 +0200

    deleted class

---
+++ src/mvc/ManagementTableView.java
@@ -0,0 +1,35 @@
+package mvc;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+
+
+public class ManagementTableView extends JFrame {
+
+
+	private static final long serialVersionUID = 1L;
+
+
+
+	public ManagementTableView()
+	{
+		
+
+		
+	}
+}

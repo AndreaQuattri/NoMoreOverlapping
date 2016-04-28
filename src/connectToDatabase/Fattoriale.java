9d751a070fefb8282b42d7e09d45fb7b9b79ec27
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-04-28 10:47:46 +0200

    added class Fattoriale
    This class has a method that returns the factorial of a number

---
+++ src/connectToDatabase/Fattoriale.java
@@ -0,0 +1,18 @@
+package connectToDatabase;
+
+public class Fattoriale {
+	
+	
+	
+	public static int fattoriale(int x){
+		int i;
+	    int f=1;
+
+	    for(i=1; i<=x; i=i+1) {
+	      f=f*i;
+	    }
+
+	    return f;
+	}
+
+}

b2857dc9e42636cfed33c956625333adcb7b2127
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-04-28 10:49:54 +0200

    Update class PianoDiStudio (field and toString)
    Added a field arrayList<Disciplina> that contains the Discipline of a
    correct Piano di Studio.
    Added toString that shows all field of the class

--- src/myComponents/PianoDiStudi.java
+++ src/myComponents/PianoDiStudi.java
@@ -7,23 +7,56 @@ public class PianoDiStudi {
 	private CorsoDiStudi corso;
 	private ArrayList<Attività> elencoAttivitàObbligatorie;
 	private ArrayList<Attività> elencoAttivitàOpzionali;
+	
+	
+	//dovrà andare qui il campo di CorsoDiStudi
+	private ArrayList<Disciplina> elencoPianiPossibili;
+
 
 	public PianoDiStudi(CorsoDiStudi corso, ArrayList<Attività> elencoAttivitàObbligatore,
 			ArrayList<Attività> elencoAttivitàOpzionali) {
 		this.corso = corso;
 		this.elencoAttivitàObbligatorie = elencoAttivitàObbligatore;
 		this.elencoAttivitàOpzionali = elencoAttivitàOpzionali;
+		this.elencoPianiPossibili = new ArrayList<Disciplina>();
 	}
 
 	public PianoDiStudi(CorsoDiStudi corso) {
 		this.corso = corso;
 		elencoAttivitàObbligatorie = new ArrayList<Attività>();
 		elencoAttivitàOpzionali = new ArrayList<Attività>();
+		this.elencoPianiPossibili = new ArrayList<Disciplina>();
 	}
 
 	@Override
 	public String toString() {
-		return corso.toString();
+		
+		//if (corso.getTipoLaurea().equals("Magistale")){
+			
+		int totC = 0;
+		
+		String toRet = corso.toString() + "\n";
+		
+		toRet += "Obbligatorie\n";
+		for (int i=0; i<elencoAttivitàObbligatorie.size(); i++){
+			toRet += elencoAttivitàObbligatorie.get(i).toString() + "\n";
+			totC += ((Disciplina)elencoAttivitàObbligatorie.get(i)).getCrediti();
+		}
+		
+		
+		toRet += "\nOpzionale\n";
+		for (int i=0; i<elencoAttivitàOpzionali.size(); i++)
+			toRet += elencoAttivitàOpzionali.get(i).toString() + "\n";
+		
+		
+
+		toRet += "\nPiani possibili\n";
+		for (int i=0; i<elencoPianiPossibili.size(); i++)
+			toRet += elencoPianiPossibili.get(i).toString() + "\n";
+		
+		return toRet+"\n"+totC;
+		//}
+		//return "";
 	}
 
 
@@ -52,6 +85,14 @@ public class PianoDiStudi {
 	
 	public void aggiungiAttivitàOpzionale(Attività attivitàOpzionale){
 		this.elencoAttivitàOpzionali.add(attivitàOpzionale);
+	}
+
+	public ArrayList<Disciplina> getElencoPianiPossibili() {
+		return elencoPianiPossibili;
+	}
+
+	public void setElencoPianiPossibili(ArrayList<Disciplina> elencoPianiPossibili) {
+		this.elencoPianiPossibili = elencoPianiPossibili;
 	}
 
 

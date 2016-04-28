af065f63ebab9d6c42cbc3a0994d510aa5806095
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-04-28 10:46:19 +0200

    added a class Disp -> Disposizioni
    this class has a method that returns the disposition of an Integer
    array. This class (and method) is not used.

---
+++ src/provaPermutazione/Disp.java
@@ -0,0 +1,42 @@
+package provaPermutazione;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+class Disp {
+
+	//disposizioni semplici di n elementi su k posizioni
+	public static void dispSemplici(int[] arr, int k, ArrayList<char[]> out) {
+		dispSemplici(arr,k,out,new String());	
+	}
+
+	//metodo di supporto per dispSemplici
+	public static void dispSemplici(int[] arr, int k, ArrayList<char[]> out, String a) {
+		if (a.length() == k) {
+			char[] temp = a.toCharArray();
+			out.add(temp);
+			//System.out.println("debug "); //debug
+			//stampaArrayList(out); //debug
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			dispSemplici(arr,k,out,a+arr[i]);
+		}
+	}
+
+	//metodo per la stampa di ArrayList<char[]>
+	public static void stampaArrayList(ArrayList<char[]> arrList) {
+		for (char[] temp : arrList)
+			System.out.println(Arrays.toString(temp));
+	}
+
+	//metodo main di prova
+	public static void main(String[] args) {
+		int[] alf = {1,2,3,4};
+		ArrayList<char[]> out = new ArrayList<char[]>();
+		dispSemplici(alf,2,out);
+		System.out.println("dispSemplici");
+		stampaArrayList(out);
+		System.out.println(out.size());
+	}
+}

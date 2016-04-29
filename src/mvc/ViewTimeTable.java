eedc829c886d712fba7f304fa8fbe20fca02c0fe
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-04-29 21:14:41 +0200

    added new View, ViewTimeTable
    In this view there will be the timetable without overlapping

--- src/mvc/ViewTimeTable.java
+++ src/mvc/ViewTimeTable.java
@@ -4,39 +4,47 @@ import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.JFrame;
 import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
-import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
-public class ViewTimeTable extends JFrame implements Observer  {
+public class ViewTimeTable extends JFrame implements Observer {
 
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-
+	
+	
+	
 	private JFrame frame;
-
-
-	private Model model;
-	private JTextField textFieldCaricaFile;
-	private JTable table;
+	private JButton buttonInsertNewTable;
 	private JButton buttonNewOrario;
-	private JButton buttonCaricaOrario;
-	private JButton buttonBrowse;
-	private JButton buttonGeneraDaCaricato;
+	private JButton buttonLoadOrario;
+	private JButton buttonGestioneOrario;
+	private JLabel labelTableInTable;
+
+
+	private JScrollPane scrollPane;
+	private DefaultTableModel tableRecords;
+	private JTable table;
+	
+	
+	private Model model;
+	
 	
 	public ViewTimeTable(Model model) {
-		
-		
-		this.setModel(model);
-		initUI();
 
-		
+		this.model = model;
+		initUI();
 		
 		// View as observer of model
 		model.addObserver(this);
@@ -44,113 +52,171 @@ public class ViewTimeTable extends JFrame implements Observer  {
 		// Init view like model
 		initViewLikeModel();
 
+	
 	}
 
-	private void initViewLikeModel() {
+	
+
+	
+	public void pressButtonGestisciOrario(ActionListener listener) {
 		// TODO Auto-generated method stub
+		buttonGestioneOrario.addActionListener(listener);
 		
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initUI() {
+	
+	
+	
+	private void initUI(){
 		
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 900, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		buttonNewOrario = new JButton("Nuovo orario");
+		buttonInsertNewTable = new JButton("Modifica esistente");
 		
-		buttonCaricaOrario = new JButton("Carica orario");
+		buttonNewOrario = new JButton("Nuovo Orario");
 		
-		textFieldCaricaFile = new JTextField();
-		textFieldCaricaFile.setColumns(10);
+		buttonLoadOrario = new JButton("Carica orario");
 		
-		buttonBrowse = new JButton("Browse");
+		scrollPane = new JScrollPane();
 		
-		buttonGeneraDaCaricato = new JButton("Genera nuovo da caricato");
+		buttonGestioneOrario = new JButton("Gestione orario");
 		
+		labelTableInTable = new JLabel("");
+		
+
+		tableRecords = new DefaultTableModel(0, 0);
 		table = new JTable();
+		table.setModel(tableRecords);
+
+
+		
+		scrollPane.setViewportView(table);
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(36)
-					.addComponent(buttonNewOrario, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(36)
-					.addComponent(buttonCaricaOrario, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE)
-					.addGap(128)
-					.addComponent(textFieldCaricaFile, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(22)
-					.addComponent(buttonBrowse, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(156)
-					.addComponent(buttonGeneraDaCaricato, GroupLayout.PREFERRED_SIZE, 269, GroupLayout.PREFERRED_SIZE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(49)
-					.addComponent(table, GroupLayout.PREFERRED_SIZE, 684, GroupLayout.PREFERRED_SIZE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(326)
+							.addComponent(buttonGestioneOrario, GroupLayout.PREFERRED_SIZE, 230, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(114)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(buttonLoadOrario, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE)
+								.addComponent(buttonNewOrario, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(66)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 775, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(193)
+									.addComponent(buttonInsertNewTable, GroupLayout.PREFERRED_SIZE, 335, GroupLayout.PREFERRED_SIZE)))))
+					.addContainerGap(59, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(20)
+					.addGap(52)
 					.addComponent(buttonNewOrario)
-					.addGap(31)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(buttonCaricaOrario)
-						.addComponent(textFieldCaricaFile, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(buttonBrowse))
-					.addGap(55)
-					.addComponent(buttonGeneraDaCaricato)
-					.addGap(61)
-					.addComponent(table, GroupLayout.PREFERRED_SIZE, 374, GroupLayout.PREFERRED_SIZE))
+					.addGap(22)
+					.addComponent(buttonLoadOrario)
+					.addGap(124)
+					.addComponent(buttonInsertNewTable)
+					.addGap(51)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 228, GroupLayout.PREFERRED_SIZE)
+					.addGap(69)
+					.addComponent(buttonGestioneOrario))
 		);
 		frame.getContentPane().setLayout(groupLayout);
-	}
-
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
 		
-	}
 
-	public Model getModel() {
-		return model;
-	}
-
-	public void setModel(Model model) {
-		this.model = model;
 	}
 	
-	public void clearUserInput() {
-		textFieldCaricaFile.setText("");
-	}
-
-	public void appendUserInput(String line) {
-		//textFieldCaricaFile.append(line);
-		//textFieldCaricaFile.setCaretPosition(textFieldCaricaFile.getDocument().getLength());
-		
-	}
-
-
-
-	public void pressButtonNewOrario(ActionListener listener) {
-		buttonNewOrario.addActionListener(listener);
-	}
-	
-	
-	public void addLoadFromFileListener(ActionListener listener) {
-		buttonBrowse.addActionListener(listener);
-	}
-	
-	public JFrame getFrame() {
+	public JFrame getFrame(){
 		return frame;
 	}
+	
+	private void initViewLikeModel()
+	{
+		
+		//buttonLoadOrario.setEnabled(model.isEnableModificaTable());
+		//buttonNewOrario.setEnabled(model.isEnableModificaTable());
 
-	public void setFrame(JFrame frame) {
-		this.frame = frame;
+	}
+	
+	private void enableUpdateTable()
+	{
+		buttonLoadOrario.setEnabled(model.isEnableModificaTable());
 	}
 
+	private void enableDeleteTable()
+	{
+		buttonNewOrario.setEnabled(model.isEnableEliminaTable());
+
+	}
+	
+
+	
+	// Update the view with the notify send by model
+		@Override
+		public void update(Observable o, Object arg)
+		{
+			MyNotify notify = (MyNotify) arg;
+
+			switch (notify.getNotifyID())
+			{
+			case MyNotify.ENABLE_BUTTON_MODIFICA:
+				enableUpdateTable();
+			break;
+			case MyNotify.ENABLE_BUTTON_ELIMINA:
+				enableDeleteTable();
+			break;
+
+
+				
+				
+			}
+
+		}
+
+		
+
+		public void setComboBoxTable(JComboBox comboBoxTable) {
+		}
+
+		public JTable getTable() {
+			return table;
+		}
+
+		public void setTable(JTable table) {
+			this.table = table;
+		}
+
+		public DefaultTableModel getTableRecords() {
+			return tableRecords;
+		}
+
+		public void setTableRecords(DefaultTableModel tableRecords) {
+			this.tableRecords = tableRecords;
+		}
+
+		
+		public JLabel getLabelTableInTable() {
+			return labelTableInTable;
+		}
+
+		public void setLabelTableInTable(JLabel labelTableInTable) {
+			this.labelTableInTable = labelTableInTable;
+		}
+
+
+
+
+		public void pressButtonNewOrario(ActionListener listener) {
+			// TODO Auto-generated method stub
+			buttonNewOrario.addActionListener(listener);
+		}
 }

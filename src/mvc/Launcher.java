d8df7f23bbf6cff933d414ae65802c8c72902541
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-05-02 23:35:43 +0200

    Update class Launcher
    There is an instruction commented

--- src/mvc/Launcher.java
+++ src/mvc/Launcher.java
@@ -7,15 +7,20 @@ public class Launcher
 	{
 		
 				Model model = new Model();
-				
+
 				MainView view = new MainView(model);
 				ViewTimeTable viewOrario = new ViewTimeTable(model);
-				
+
 				@SuppressWarnings("unused")
 				Controller controller = new Controller(model, view, viewOrario);
-				
+
 				view.getFrame().setVisible(true);
-			
+
+		 
+		
+		//ProvaJTable prova = new ProvaJTable();
+		
+
 	}
 
 }

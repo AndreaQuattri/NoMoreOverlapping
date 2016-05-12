a235d8b8cba70e490be73e0044b11d472fe31a74
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-05-12 16:22:29 +0200

    added an import

--- src/connectToDatabase/CreateTimeTable.java
+++ src/connectToDatabase/CreateTimeTable.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import mvc.Model;
 import myComponents.Assegnamento;
+import myComponents.Attività;
 import myComponents.Orario;
 
 public class CreateTimeTable {
@@ -21,6 +22,7 @@ public class CreateTimeTable {
 
 
 		ArrayList<Assegnamento> listAssegnamento;
+		ArrayList<Attività> listAttività;
 		ArrayList<String> listGiorni = new ArrayList<String>();
 		listGiorni.add("Lunedi");
 		listGiorni.add("Martedi");
@@ -33,7 +35,7 @@ public class CreateTimeTable {
 		for (int iFascia = 0; iFascia<model.getListFasciaOraria().size(); iFascia++){
 
 			listAssegnamento = new ArrayList<Assegnamento>();
-
+			listAttività = new ArrayList<Attività>();
 
 			for (int iGiorni = 0; iGiorni<listGiorni.size(); iGiorni++){
 
@@ -41,13 +43,12 @@ public class CreateTimeTable {
 				for (int iNumAssegnamento = 0; iNumAssegnamento<model.getListAssegnamento().size(); iNumAssegnamento++){
 
 					for (int iAssegnamento = 0; iAssegnamento<model.getListAssegnamento().get(iNumAssegnamento).size(); iAssegnamento++){
-
-						//System.out.println(model.getListAssegnamento().get(iNumAssegnamento).get(iAssegnamento).toString());
 						
 						if (model.getListFasciaOraria().get(iFascia).equals(model.getListAssegnamento().get(iNumAssegnamento).get(iAssegnamento).getFasciaOraria())){
 
 							if(model.getListAssegnamento().get(iNumAssegnamento).get(iAssegnamento).getFasciaOraria().getGiorno().equals(listGiorni.get(iGiorni))){
 								listAssegnamento.add(model.getListAssegnamento().get(iNumAssegnamento).get(iAssegnamento));
+								listAttività.add(model.getListAssegnamento().get(iNumAssegnamento).get(iAssegnamento).getAttività());
 							}
 
 						}
@@ -56,7 +57,7 @@ public class CreateTimeTable {
 
 			}
 
-			model.getListOrario().add(new Orario(listAssegnamento));
+			model.getListOrario().add(new Orario(listAssegnamento,listAttività));
 
 		}
 
@@ -69,11 +70,13 @@ public class CreateTimeTable {
 
 
 		ArrayList<Assegnamento> listAssegnamento;
+		ArrayList<Attività> listAttività;
 
 
 		for (int iFascia = 0; iFascia<model.getListFasciaOraria().size(); iFascia++){
 
 			listAssegnamento = new ArrayList<Assegnamento>();
+			listAttività = new ArrayList<Attività>();
 
 			for (int iNumAssegnamento = 0; iNumAssegnamento<model.getListAssegnamento().size(); iNumAssegnamento++){
 
@@ -81,13 +84,14 @@ public class CreateTimeTable {
 
 					if (model.getListFasciaOraria().get(iFascia).equals(model.getListAssegnamento().get(iNumAssegnamento).get(iAssegnamento).getFasciaOraria())){
 						listAssegnamento.add(model.getListAssegnamento().get(iNumAssegnamento).get(iAssegnamento));
+						listAttività.add(model.getListAssegnamento().get(iNumAssegnamento).get(iAssegnamento).getAttività());
 					}
 
 				}
 
 			}
 
-			model.getListOrario().add(new Orario(listAssegnamento));
+			model.getListOrario().add(new Orario(listAssegnamento,listAttività));
 
 		}
 

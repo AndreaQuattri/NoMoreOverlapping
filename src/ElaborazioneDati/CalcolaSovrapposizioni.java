1cca14af9b5c7b2ed100a7ac0ccb485fa5133f3b
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-05-16 10:32:56 +0200

    added class CalcolaSovrapposizioni
    This class must be implemented

--- src/ElaborazioneDati/CalcolaSovrapposizioni.java
+++ src/ElaborazioneDati/CalcolaSovrapposizioni.java
@@ -1,47 +1,33 @@
 package ElaborazioneDati;
 
-import java.util.ArrayList;
 
 import mvc.Model;
-import myComponents.Attività;
-import myComponents.CorsoDiStudi;
-import myComponents.Disciplina;
-import myComponents.PianoDiStudi;
 
 public class CalcolaSovrapposizioni {
 
 	private Model model;
 
 	public CalcolaSovrapposizioni(Model model) {
-		this.model = model;
+		this.setModel(model);
 	}
 
 
 	public int numSovrapposizioni (){
 
 
-		ArrayList<Disciplina> listSovrapposizione = new ArrayList<Disciplina>();
-		int k;
-
-		for (int i=0; i<model.getListOrario().size(); ){
-			for (int j=0; j<model.getListOrario().get(i).getElencoAssegnamenti().size(); j++){
-				System.out.println(model.getListOrario().get(i).getElencoAssegnamenti().get(j).getAttività().toString() +
-						model.getListOrario().get(i).getElencoAssegnamenti().get(j).getFasciaOraria().toString());
-				
-				
-				while (!model.getListOrario().get(i).getElencoAssegnamenti().equals(model.getListOrario().get(i+1).getElencoAssegnamenti())){
-					System.out.println(model.getListOrario().get(i).getElencoAssegnamenti());
-					System.out.println(model.getListOrario().get(i+1).getElencoAssegnamenti());
-
-					i++;
-				}
-				
-			}
-			System.out.println();
-		}
-
+		
 
 		return 0;
+	}
+
+
+	public Model getModel() {
+		return model;
+	}
+
+
+	public void setModel(Model model) {
+		this.model = model;
 	}
 
 }

60c3c9ebae1ee0faa3e9917cb4e5e2394a4d3732
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-05-28 20:39:29 +0200

    Update class InsertInTimeTable
    Added code to insert an Activity in time table

--- src/controllerListener/InsertInTimeTable.java
+++ src/controllerListener/InsertInTimeTable.java
@@ -2,26 +2,33 @@ package controllerListener;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Vector;
 
+import connectToDatabase.CreateTimeTable;
 import connectToDatabase.DisciplinaInseritaPiano;
 import mvc.Model;
 import mvc.UpdateTimeTable;
+import mvc.ViewTimeTable;
+import myComponents.Assegnamento;
 import myComponents.Attività;
+import myComponents.Orario;
 
 public class InsertInTimeTable implements ActionListener{
 	
 	private Model model;
 	private UpdateTimeTable updateTimeTable;
+	private ViewTimeTable viewOrario;
 	
 	
-	public InsertInTimeTable(Model model, UpdateTimeTable updateTimeTable) {
+	public InsertInTimeTable(Model model, ViewTimeTable viewOrario, UpdateTimeTable updateTimeTable) {
 		this.setModel(model);
 		this.setUpdateTimeTable(updateTimeTable);
+		this.viewOrario = viewOrario;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
 		System.out.println("Button pressed");
 		
@@ -31,10 +38,39 @@ public class InsertInTimeTable implements ActionListener{
 		
 		DisciplinaInseritaPiano d = new DisciplinaInseritaPiano(model);
 		Attività a = model.getAttivitàFromId(Integer.parseInt((id[0]).trim()));
-		System.out.println(d.getGiorno(updateTimeTable.getRow()));
+		System.out.println(d.getGiorno(updateTimeTable.getCol()-1));
+		System.out.println(d.getTime(updateTimeTable.getRow()));
+		int i;
 		
-		System.out.println(d.èVuotaeCiSta(a.getOre(), updateTimeTable.getRow(),
-				updateTimeTable.getCol(), model.getMatrix(), 0));
+		if(d.èVuotaeCiSta(a.getOre(), updateTimeTable.getRow(),
+				updateTimeTable.getCol(), model.getMatrix(), 100)){
+			
+			for (i=0; i<model.getListFasciaOraria().size(); i++)
+				if (model.getListFasciaOraria().get(i).getGiorno().equals(d.getGiorno(updateTimeTable.getCol()-1)) &&
+						model.getListFasciaOraria().get(i).getInizio().toString().substring(11, 19).equals(d.getTime(updateTimeTable.getRow()))){
+					break;
+				}
+			
+			
+			int hour=0;
+			while(hour<a.getOre()){
+				model.getListAssegnamento().add(new Assegnamento(a, model.getListFasciaOraria().get(i), model.getListAula().get(0)));
+				hour++;
+				i++;
+				
+			}
+		}
+		
+		CreateTimeTable c = new CreateTimeTable(model);
+		
+		model.setListOrario(new ArrayList<Orario>());
+		c.fromAssegnamentoToOrarioPerGiorno();
+		
+		model.setTabella(new Vector<Vector<String>>());
+		model.fromOrarioToTable();
+		viewOrario.visualizzaOrario();
+		
+		
 		
 		
 		

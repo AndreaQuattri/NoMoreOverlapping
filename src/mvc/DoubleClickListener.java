2b1c3bc53d369ca713f28f759b35a332c242e7fc
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-05-31 00:01:16 +0200

    Update class DoubleClickListener
    In constructor I deleted comment and systemOut of check.
    In constructor I added a check on cell of JTable

--- src/mvc/DoubleClickListener.java
+++ src/mvc/DoubleClickListener.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseListener;
 import javax.swing.JTable;
 
 import controllerListener.InsertInTimeTable;
+import controllerListener.SelectedGitaListener;
 
 
 public class DoubleClickListener implements MouseListener {
@@ -23,73 +24,30 @@ public class DoubleClickListener implements MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		//se non è doppio click ritorno		
+		
 
-
-
-		System.out.println("Nella funzione");
-
-		/*
-		if(e.getClickCount() < 2){
-			System.out.println("One click");
-			return;
-		}
-		 */
-
-		/*
-
-		//Considero solo i click del pulsante sinistro;
-		//se vuoi gestire l'evento anche se si fa dopppio
-		//click col tasto destro commenta questo if
-		if(e.getButton() != MouseEvent.BUTTON1){
-			System.out.println("Tasto destro");
-			return;
-		}
-
-		 */
-
+		
 
 		//Ricavo la tabella che ha prodotto l'evento
 		JTable table = (JTable)e.getSource();
 		//Calcolo la riga su cui si è fatto click
 		int clickedRow = table.rowAtPoint(e.getPoint());
 		int clickedCol = table.columnAtPoint(e.getPoint());
-		//Se non è la riga che mi interessa ritorno
 
 
-
-		UpdateTimeTable updateTimeTable = new UpdateTimeTable(model, clickedRow, clickedCol);
-		updateTimeTable.setVisible(true);
-
-		
-		updateTimeTable.selectedGita(new SelectedGitaListener());
-		updateTimeTable.insertGitaConvegno(new InsertInTimeTable(model, viewOrario , updateTimeTable));
+		if (!(clickedCol == 6 && clickedRow>9)){
+			UpdateTimeTable updateTimeTable = new UpdateTimeTable(model, clickedRow, clickedCol, viewOrario.getButtonSem1().isSelected()==true? 1 : 2);
+			updateTimeTable.setVisible(true);
 
 
-	}
-
-
-	private class SelectedGitaListener implements  ActionListener
-	{
-		@Override
-		public void actionPerformed(ActionEvent action)
-		{
-
-
-			model.enableButtonInserisciGita(true);
-
-			//TODO
-			/*
-			 * 
-			 * cancellare tabella, ecc..
-			 * 
-			 */
-
-
-
+			updateTimeTable.selectedGita(new SelectedGitaListener(model));
+			updateTimeTable.insertGitaConvegno(new InsertInTimeTable(model, viewOrario , updateTimeTable));
 		}
+
 	}
+
+
+	
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {

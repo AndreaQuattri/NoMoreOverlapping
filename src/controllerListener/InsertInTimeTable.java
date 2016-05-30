85b7f408cdf642d260fb7237acd90ad8e53500d1
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-05-30 23:54:41 +0200

    completed class InsertInTimeTable

--- src/controllerListener/InsertInTimeTable.java
+++ src/controllerListener/InsertInTimeTable.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Vector;
 
+import javax.swing.JCheckBoxMenuItem;
+
 import connectToDatabase.CreateTimeTable;
 import connectToDatabase.DisciplinaInseritaPiano;
 import mvc.Model;
@@ -15,12 +17,12 @@ import myComponents.Attività;
 import myComponents.Orario;
 
 public class InsertInTimeTable implements ActionListener{
-	
+
 	private Model model;
 	private UpdateTimeTable updateTimeTable;
 	private ViewTimeTable viewOrario;
-	
-	
+
+
 	public InsertInTimeTable(Model model, ViewTimeTable viewOrario, UpdateTimeTable updateTimeTable) {
 		this.setModel(model);
 		this.setUpdateTimeTable(updateTimeTable);
@@ -29,52 +31,70 @@ public class InsertInTimeTable implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		System.out.println("Button pressed");
-		
+
+
 		String attivitàSelected = (String)updateTimeTable.getComboGita().getSelectedItem();
-		
+
 		String[] id = attivitàSelected.split("-");
-		
+
 		DisciplinaInseritaPiano d = new DisciplinaInseritaPiano(model);
-		Attività a = model.getAttivitàFromId(Integer.parseInt((id[0]).trim()));
-		System.out.println(d.getGiorno(updateTimeTable.getCol()-1));
-		System.out.println(d.getTime(updateTimeTable.getRow()));
+		Attività a;
+		a = model.getAttivitàFromId((id[0]).trim());
 		int i;
-		
-		if(d.èVuotaeCiSta(a.getOre(), updateTimeTable.getRow(),
-				updateTimeTable.getCol(), model.getMatrix(), 100)){
-			
+
+		if(d.èVuotaeCiSta(a.getOre()*2, updateTimeTable.getRow(),
+				updateTimeTable.getCol(), model.getMatrix(), 100, false) &&
+				!d.giàOccupataDaStessoProf(a, updateTimeTable.getRow(), updateTimeTable.getCol()-1)){
+
 			for (i=0; i<model.getListFasciaOraria().size(); i++)
 				if (model.getListFasciaOraria().get(i).getGiorno().equals(d.getGiorno(updateTimeTable.getCol()-1)) &&
 						model.getListFasciaOraria().get(i).getInizio().toString().substring(11, 19).equals(d.getTime(updateTimeTable.getRow()))){
 					break;
 				}
-			
-			
+
+
 			int hour=0;
-			while(hour<a.getOre()){
+			while(hour<(a.getOre()*2)){
 				model.getListAssegnamento().add(new Assegnamento(a, model.getListFasciaOraria().get(i), model.getListAula().get(0)));
 				hour++;
 				i++;
-				
+
 			}
+
+
+			if(!model.getListAttivitàInserite().contains(a)){
+				model.getListAttivitàInserite().add(a);
+				viewOrario.setButtonCheckBox(new JCheckBoxMenuItem(a.getNome()));
+				viewOrario.getVisualizzaAttività().add(viewOrario.getButtonCheckBox());
+				viewOrario.getButtonCheckBox().addActionListener(new SelectedActivityListener(model, viewOrario,
+						a.getNome()));
+			}
+
+			for (i=0; i<a.getElencoResponsabili().size(); i++){
+				if(!model.getListDocentiInseriti().contains(a.getElencoResponsabili().get(i))){
+					model.getListDocentiInseriti().add(a.getElencoResponsabili().get(i));
+					viewOrario.setButtonCheckBox(new JCheckBoxMenuItem(a.getElencoResponsabili().get(i).getMatricola() + " - " + 
+							a.getElencoResponsabili().get(i).getCognome()));
+					viewOrario.getVisualizzaDocente().add(viewOrario.getButtonCheckBox());
+					viewOrario.getButtonCheckBox().addActionListener(new SelectedDocenteListener(model, viewOrario, 
+							a.getElencoResponsabili().get(i).getMatricola()));
+				}
+			}
+
+			CreateTimeTable c = new CreateTimeTable(model);
+
+			model.setListOrario(new ArrayList<Orario>());
+			c.fromAssegnamentoToOrarioPerGiorno();
+
+			model.setTabella(new Vector<Vector<String>>());
+			model.fromOrarioToTable();
+			viewOrario.visualizzaOrario();
+
+
+
 		}
-		
-		CreateTimeTable c = new CreateTimeTable(model);
-		
-		model.setListOrario(new ArrayList<Orario>());
-		c.fromAssegnamentoToOrarioPerGiorno();
-		
-		model.setTabella(new Vector<Vector<String>>());
-		model.fromOrarioToTable();
-		viewOrario.visualizzaOrario();
-		
-		
-		
-		
-		
-		
+
+
 	}
 
 	public Model getModel() {

28fc5a03c53c3d98e7f538fe86b6a9cfd5e1b20f
Andrea Quattri <a.quattri@studenti.unibg.it>
2016-06-21 18:43:11 +0200

    Update class to save and load file

--- src/controllerListener/CaricaOrarioDaFile.java
+++ src/controllerListener/CaricaOrarioDaFile.java
@@ -2,44 +2,74 @@ package controllerListener;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
 import javax.xml.stream.XMLStreamException;
+
+import connectToDatabase.MyFileFilter;
 import mvc.Model;
 import xmlParser.XMLOrarioParserDOM;
 
-public class CaricaOrarioDaFile implements ActionListener{
+public class CaricaOrarioDaFile extends JPanel implements ActionListener{
 
 
 	Model model;
-	
+
 	public CaricaOrarioDaFile( Model model ) {
 		this.model = model;
 	}
 
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
-		String file = "provaorario.xml";
-		System.out.println("Pressed button carica");
-		try {
-			model.setOrarioUfficiale( XMLOrarioParserDOM.getOrarioFromFileFull(file) );
-			
-			
-			for (int i=0; i<model.getOrarioUfficiale().getElencoAssegnamenti().size(); i++)
-				System.out.println(model.getOrarioUfficiale().getElencoAssegnamenti().get(i).getAttività().toString());
-			
-			model.fromOrarioToTable();
-		
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (XMLStreamException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+
+
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogTitle("Select a Time Table");
+		fileChooser.setApproveButtonText("Open");
+		fileChooser.setCurrentDirectory(new java.io.File("."));
+		fileChooser.setFileFilter(new MyFileFilter());
+
+
+		int n = fileChooser.showOpenDialog(CaricaOrarioDaFile.this);
+
+		if (n == JFileChooser.APPROVE_OPTION)
+		{
+
+			File inputFile = fileChooser.getSelectedFile();
+
+			String file = inputFile.getName();
+			System.out.println(file);
+
+
+
+
+
+			try {
+				model.setOrarioUfficiale( XMLOrarioParserDOM.getOrarioFromFileFull(file) );
+
+
+				for (int i=0; i<model.getOrarioUfficiale().getElencoAssegnamenti().size(); i++)
+					System.out.println(model.getOrarioUfficiale().getElencoAssegnamenti().get(i).getAttività().toString());
+
+				model.fromOrarioToTable();
+
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (XMLStreamException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
 		}
-		
+
+
+
 	}
 
 }
--- src/controllerListener/SalvaOrarioEsistente.java
+++ src/controllerListener/SalvaOrarioEsistente.java
@@ -3,6 +3,10 @@ package controllerListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import mvc.Model;
 import myComponents.Orario;
 import xmlParser.XMLOrarioParserDOM;
@@ -10,22 +14,48 @@ import xmlParser.XMLOrarioParserDOM;
 public class SalvaOrarioEsistente implements ActionListener{
 
 	Model model;
-	
+
 	public SalvaOrarioEsistente( Model model ) {
 		this.model = model;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
+
+		/*
+
 		// TODO Auto-generated method stub
 		String file = "provaorario.xml";
-		
+
 		for (int i=0; i<model.getOrarioUfficiale().getElencoAssegnamenti().size(); i++)
 			System.out.println(model.getOrarioUfficiale().getElencoAssegnamenti().get(i).getFasciaOraria().toString() +"\n"+
 								model.getOrarioUfficiale().getElencoAssegnamenti().get(i).getAttività().toString());
 
-		
+
 		XMLOrarioParserDOM.writeOrarioOnFileFull( file , this.model.getOrarioUfficiale() );
+
+		 */
+		//JFileChooser chooser = new JFileChooser();
+		//int s = chooser.showSaveDialog(new JFrame());
+
+
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				".xml", "xml", "gif");
+		chooser.setFileFilter(filter);
+		chooser.setCurrentDirectory(new java.io.File("."));
+
+		int returnVal = chooser.showSaveDialog(new JFrame());
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			System.out.println("You chose to open this file: " +
+					chooser.getSelectedFile().getName());
+			
+		}
+
+		XMLOrarioParserDOM.writeOrarioOnFileFull( chooser.getSelectedFile().getName() , this.model.getOrarioUfficiale() );
+
+
+
 	}
 
 }
